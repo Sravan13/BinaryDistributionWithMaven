c6946891ecfdb07817b02da504ae27108beca187
Sravan13 <chithari.sravan@gmail.com>
2017-11-12 00:06:34 +0530

    first commit

---
+++ src/main/java/com/sravan/binaryDist/App.java
@@ -0,0 +1,17 @@
+package com.sravan.binaryDist;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static Logger LOGGER = Logger.getLogger(App.class);
+	 
+    public static void main( String[] args )
+    {
+        LOGGER.info("Hello World!");
+    }
+}
